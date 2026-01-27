5099f6e32ecac290368ef95ee2d455f4105d0186
Yurii Tiytiynnyk <foenix.t2@gmail.com>
2026-01-28 00:46:53 +0100

    Make system bars transparent

--- app/src/main/java/com/example/quantiq/ui/MainActivity.kt
+++ app/src/main/java/com/example/quantiq/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.quantiq.ui
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.mutableStateOf
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.quantiq.QuantiqApplication
 import com.example.quantiq.ui.settings.notifications.NotificationDetailsViewModelFactory
 import com.example.quantiq.ui.settings.notifications.NotificationsSettingsViewModel
@@ -27,6 +29,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
+        }
 
         val appContainer = (application as QuantiqApplication).appContainer
         appContainer.billingManager.startConnection()
